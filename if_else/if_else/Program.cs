a544579077d3c7d7fe60191b4765f2e781f12a14
XeoPlay <xeoplay123456@gmail.com>
2020-05-16 22:29:19 +0300

    learned if and else

---
+++ if_else/if_else/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double input1 = double.Parse(input);
+
+            if (input1 > 4)
+            {
+                Console.WriteLine("Number is more than 4");
+            }
+            else if (input1 < 4)
+            {
+                Console.WriteLine("Number is less than 4");
+            }
+            else
+            {
+                Console.WriteLine("Number is equal to 4");
+            }
+        }
+    }
+}
