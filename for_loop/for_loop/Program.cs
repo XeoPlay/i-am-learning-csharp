8689fc0c69de963208190adc282a61cd2068d2e8
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 17:30:28 +0300

    learned for loop

---
+++ for_loop/for_loop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= limit; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
