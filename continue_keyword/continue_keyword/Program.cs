41c3bb6f0a5eb9df33cc39e8eea64329f8a5cb5f
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 17:58:13 +0300

    learned what the continue keyword does

---
+++ continue_keyword/continue_keyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace continue_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++) // will output 1 3 4 5
+            {                                 
+                if (i == 2)
+                {
+                    continue;
+                }
+                
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
