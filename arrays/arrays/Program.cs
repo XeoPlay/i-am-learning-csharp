0898163c49f7a43f32c9c5f9a33533abe8916c15
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 19:17:14 +0300

    learned basic stuff about arrays

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray;
+
+            myArray = new int[5];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i + 1;
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
