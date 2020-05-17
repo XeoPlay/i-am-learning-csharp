09e1fe07926c064214edb8163d956eea17170a67
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 17:15:34 +0300

    learned do while

---
+++ do_while_loop/do_while_loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int limit = int.Parse(Console.ReadLine());
+
+            do //this is practically the same thing lol
+            {
+                count++;
+                Console.WriteLine(count);
+            } while (count < limit);
+        }
+    }
+}
