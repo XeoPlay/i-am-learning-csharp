ae2d27ad5b55264e77a878a197afd04abd6a1d93
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 17:50:04 +0300

    learned what the break keyword does

---
+++ break_keyword/break_keyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                string msg = Console.ReadLine();
+                
+                if (msg == "exit")
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
