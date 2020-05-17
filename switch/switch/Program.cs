1559b7364d94e847d1f754e1c0cddc88feab070d
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 16:38:14 +0300

    learned switch (not the nintendo one lol)

---
+++ switch/switch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("1 or 2");
+                    break;
+                
+                default:
+                    Console.WriteLine("nor 1 nor 2");
+                    break;
+            }
+        }
+    }
+}
