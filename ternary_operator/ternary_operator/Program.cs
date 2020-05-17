5441b6c27f1d438678f75dee48c230ee3de5aa02
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 18:57:09 +0300

    learned what ternary operators do (if that's what they're called)

---
+++ ternary_operator/ternary_operator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternary_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool accessAllowed = false;
+
+            string storedPassword = "qwerty";
+            string enteredPassword;
+
+            while (accessAllowed == false)
+            {
+                enteredPassword = Console.ReadLine();
+                accessAllowed = (storedPassword == enteredPassword) ? true : false;
+                if (accessAllowed == false)
+                {
+                    Console.WriteLine("nope");
+                }
+            }
+
+            Console.WriteLine("u guessed it");
+        }
+    }
+}
