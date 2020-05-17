a5bf17f92b185886943b9e23ad33975466a8af7f
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 18:15:44 +0300

    learned nested loops (if that's what they're called)

---
+++ nested_loops (if that's what they're called)/nested_loops (if that's what they're called)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_loops__if_that_s_what_they_re_called_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 10; a++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int l = 0; l < 10; l++)
+                    {
+                        Console.WriteLine(a + " " + i + " " + l); // i thik i accidentaly made a program that writes down all the 3-digit numbers lol
+                    }
+                }
+            }
+        }
+    }
+}
