6aca6c87c47fbb436a6f10e37da02bba29f7cb5c
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 16:47:57 +0300

    made calculator using switch

---
+++ homework/calculator/calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the first number:");
+            double firstNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write the second number:");
+            double secondNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write + - * or /");
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                    Console.WriteLine("Result is: " + (firstNum + secondNum));
+                    break;
+                case "-":
+                    Console.WriteLine("Result is: " + (firstNum - secondNum));
+                    break;
+                case "*":
+                    Console.WriteLine("Result is: " + (firstNum * secondNum));
+                    break;
+                case "/":
+                    Console.WriteLine("Result is: " + (firstNum / secondNum));
+                    break;
+            }
+        }
+    }
+}
