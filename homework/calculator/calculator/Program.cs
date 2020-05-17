9860d0446dc855603561efdd2a6a5ddc93ada75a
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 16:51:33 +0300

    updated calculator

--- homework/calculator/calculator/Program.cs
+++ homework/calculator/calculator/Program.cs
@@ -33,6 +33,9 @@ namespace calculator
                 case "/":
                     Console.WriteLine("Result is: " + (firstNum / secondNum));
                     break;
+                default:
+                    Console.WriteLine("Wrong symbol(");
+                    break;
             }
         }
     }
