15f1e3aee26079ef2a564cf54cd7047dc37840d8
XeoPlay <xeoplay123456@gmail.com>
2020-05-17 11:19:12 +0300

    learned shortened operators of logic

---
+++ shortened_operators_of_logic/shortened_operators_of_logic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shortened_operators_of_logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fanspeed = 0;
+            bool isHighTemp = true;
+            bool hasNoCooling = fanspeed <= 0;
+
+            if (isHighTemp || hasNoCooling)
+            {
+                Console.WriteLine("Proccesor could be overheated");
+            }
+        }
+    }
+}
