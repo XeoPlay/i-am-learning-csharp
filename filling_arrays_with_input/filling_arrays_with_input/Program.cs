c5d5694709ab2c1e9d6413ca32c5263ff1de5a34
XeoPlay <xeoplay123456@gmail.com>
2020-05-21 22:19:50 +0300

    learned how to fill arrays with user input

---
+++ filling_arrays_with_input/filling_arrays_with_input/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filling_arrays_with_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many positions do you want?\t");
+
+            int pos = int.Parse(Console.ReadLine());
+
+            int[] array = new int[pos];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Enter the number for array[{i}]:\t");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Your array is:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
