39835f9c94ea37c7428c545dc855c35c4070814b
XeoPlay <xeoplay123456@gmail.com>
2020-05-21 22:07:51 +0300

    learned how to use arrays with for loop

---
+++ arrays_and_loops/arrays_and_loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5 };
+
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
