2640554876bf75064b57243f04dbb7a54d6b2447
XeoPlay <xeoplay123456@gmail.com>
2020-05-22 18:10:01 +0300

    learned what System.Linq is and what the Array class is

---
+++ using_linq/using_linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace using_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 13, 25, 7, 6, 57, 78 };
+            Array.Sort(myArray); // 6 7 13 25 57 78
+            Array.Reverse(myArray); // 78 57 25 13 7 6
+            int sum = myArray.Sum(); // 78 + 57 + 25 + 13 + 7 + 6
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]); //prints out "78 57 25 13 7 6"
+            }
+            Console.WriteLine(sum); //prints out "186"
+        }
+    }
+}
